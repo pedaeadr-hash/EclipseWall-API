25bf9f92d6703042ab576bb26bca56d0329cf810
Adrian Ariel <pedaeadr@gmail.com>
2026-07-07 20:47:41 -0300

    Organizando Controllers

--- Controllers/UserREST.cs
+++ Controllers/UserREST.cs
@@ -168,31 +168,6 @@ namespace Controles
                 var role = User.FindFirst(ClaimTypes.Role)?.Value;
             return Ok(new {email,role});
         }
-        [HttpPost("upwall")]
-        public async Task<IActionResult> uploadWall (Wallpaper wall)
-        {
-            await bank.Wallpapers.AddAsync(wall);
-            bank.SaveChanges();
-            return Ok ("upload feito com sucesso");
-        }
-        [HttpGet("wall")]
-        public async Task<IActionResult> extrairwall(int carregar = 1, int limit = 10)
-        {
-        int registrosParaPular = (carregar - 1) * limit;
-
-            // Correção: Ordenar ANTES de pular e pegar garante consistência nos dados
-        var lista = await bank.Wallpapers
-                                .OrderBy(x => x.Downloads)
-                                .Skip(registrosParaPular)
-                                .Take(limit)
-                                .ToListAsync();
-
-            int countwall = await bank.Wallpapers.CountAsync();
-
-            // Correção: Arredonda para cima para não cortar a última página incompleta
-            int limitecarregarmais = (int)Math.Ceiling((double)countwall / limit);
-
-            return Ok(new { lista, limitecarregarmais });
-        }
     }
+        
 }
--- Controllers/WallREST.cs
+++ Controllers/WallREST.cs
@@ -1,5 +1,6 @@
 using Gen;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 namespace ControllersWall
 {
     [ApiController]
@@ -17,6 +18,32 @@ namespace ControllersWall
         {
             return Ok ("funcionando");
         }
+        [HttpPost("upwall")]
+        public async Task<IActionResult> uploadWall (Wallpaper wall)
+        {
+            await Bank.Wallpapers.AddAsync(wall);
+            Bank.SaveChanges();
+            return Ok ("upload feito com sucesso");
+        }
+        [HttpGet("wall")]
+        public async Task<IActionResult> extrairwall(int carregar = 1, int limit = 10)
+        {
+        int registrosParaPular = (carregar - 1) * limit;
 
+            // Correção: Ordenar ANTES de pular e pegar garante consistência nos dados
+        var lista = await Bank.Wallpapers
+                                .OrderBy(x => x.Downloads)
+                                .Skip(registrosParaPular)
+                                .Take(limit)
+                                .ToListAsync();
+
+            int countwall = await Bank.Wallpapers.CountAsync();
+
+            // Correção: Arredonda para cima para não cortar a última página incompleta
+            int limitecarregarmais = (int)Math.Ceiling((double)countwall / limit);
+
+            return Ok(new { lista, limitecarregarmais });
+        }
     }
+
 }
