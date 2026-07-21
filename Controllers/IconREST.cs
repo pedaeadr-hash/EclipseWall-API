71b8a9513786b526e6bf97f70784b4cbf6cfe04e
Adrian Ariel <pedaeadr@gmail.com>
2026-07-20 23:54:45 -0300

    fix : endpoint SaveIcon

--- Controllers/IconREST.cs
+++ Controllers/IconREST.cs
@@ -19,17 +19,26 @@ namespace IconUser
         public async Task<IActionResult> Teste([FromBody] IconsUser Iu)
         {
     
-            try
-            {
+                bool Verificar = await Bank.IconsUsers.AnyAsync(x=>x.UserId==Iu.UserId);
+                if (Verificar==true)
+                {
+                    var objecto = await Bank.IconsUsers.FirstOrDefaultAsync(x=>x.UserId==Iu.UserId);
+                    if (objecto == null)
+                {
+                    return BadRequest();
+                }
+                    objecto.UrlIcon=Iu.UrlIcon;
+                
+                await Bank.SaveChangesAsync();
+                return Ok("Tudo Certo icon salva no banco de dados");
+                }
                 await Bank.IconsUsers.AddAsync(Iu);
                 await Bank.SaveChangesAsync();
                 return Ok("Tudo Certo icon salva no banco de dados");
-            }
-            catch (Exception ex)
-            {
-                // Retorna a mensagem real do erro para você ver no console do navegador/Postman
-                return StatusCode(500, new { erro = ex.Message, detalhe = ex.InnerException?.Message });
-            }
+           
+            
+            
+        
         }
-        }
-        }
+    }
+}
